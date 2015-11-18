9e12b39091e0cfcaa1f1b5a12e05fb1276278924
Brian Swisher <bswisher@gmail.com>
2015-11-18 01:18:57 +0000

    change comment

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@
   var express = require('express');
   var router = express.Router();
 
-  // catch all at bottom
+  // bottom catch all
   router.get('*', function(req, res) {
     res.send('deployable');
   });
