643cebcc7314197359e3827ea0e3c569713d2b79
brianswisher <bswisher@gmail.com>
2017-05-04 13:00:42 -0700

    secondary commit; test

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,17 @@
 (function() {
-  'use strict';
+  'use strict'
 
-  var express = require('express');
-  var router = express.Router();
+  var express = require('express')
+  var router = express.Router()
 
   // bottom catch all
   router.get('*', function(req, res) {
-    res.send('deployable');
+    var payload = { name: 'deployable' }
+
+    res.setHeader('Content-Type', 'application/json')
+
+    res.send(JSON.stringify(payload))
   });
 
-  module.exports = router;
-}());
+  module.exports = router
+}())
